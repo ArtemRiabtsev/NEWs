d41de109349dddb9e6b640ae80fd9e941ea7a3b1
Riabchik69000 <riabchik2013@gmail.com>
2019-02-10 16:50:08 +0200

    Initial Commit

---
+++ NEWs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NEWs
+//
+//  Created by Артем Рябцев on 2/10/19.
+//  Copyright © 2019 Артем Рябцев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
